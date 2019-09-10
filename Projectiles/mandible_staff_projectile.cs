ad2b6056c9edc82034bb8a20a2915dd4ec1f3638
pasgen <36134204+pasgen@users.noreply.github.com>
2019-09-10 19:24:25 +0300

    Add files via upload

---
+++ Projectiles/mandible_staff_projectile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.IO;
+
+namespace jam.Projectiles
+{
+    class mandible_staff_projectile : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.friendly = true;
+            projectile.thrown = true;
+            projectile.penetrate = 8;      //this is how many enemy this projectile penetrate before desapear
+            projectile.extraUpdates = 1;
+            projectile.timeLeft = 600;
+            projectile.light = 0.5f;
+            projectile.tileCollide = true;
+            Main.projFrames[projectile.type] = 4;
+        }
+
+        public override void AI()
+        {
+            projectile.ai[0] += 1f; // 60 == 1 second
+            if (projectile.ai[1] == null)
+            {
+                projectile.ai[1] = projectile.velocity.X;
+            }
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+            
+            if (!(projectile.ai[1] < 0.01f))
+            {
+                projectile.ai[1] -= 0.01f;
+            }
+            //projectile.velocity.X = projectile.ai[1];
+
+            if (Main.rand.Next(2) == 0) // this is how many duspt particles will spawn
+            {// DustID.Fire is a vanilla terrraria dust, change it to what you like. To add a modded dust the change DustID.Fire with mod.DustType("DustName")
+                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 6, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) //When you hit an NPC
+        {
+
+            target.AddBuff(24, 510);    //this adds a buff to the npc hit. 210 it the time of the buff
+
+        }
+        public override bool PreDraw(SpriteBatch sb, Color lightColor) //this is where the animation happens
+        {
+            projectile.frameCounter++; //increase the frameCounter by one
+            if (projectile.frameCounter >= 4) //once the frameCounter has reached 4 - change the 4 to change how fast the projectile animates
+            {
+                projectile.frame++; //go to the next frame
+                projectile.frameCounter = 0; //reset the counter
+                if (projectile.frame > 3) //if past the last frame
+                    projectile.frame = 0; //go back to the first frame
+            }
+            return true;
+        }
+    }
+}
