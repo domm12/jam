e558dd6eb75d73dfc5998ba193ed1f46102475b5
pasgen <36134204+pasgen@users.noreply.github.com>
2019-09-11 19:29:16 +0300

    Add files via upload

---
+++ Items/dart_machine_gun.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace jam.Items
+{
+    public class dart_machine_gun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dart Machine Gun");
+            Tooltip.SetDefault("");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 19;
+            item.ranged = true;
+            item.width = 40;
+            item.height = 20;
+            item.useTime = 8;
+            item.useAnimation = 20;
+            item.useStyle = 5;
+            item.noMelee = true; //so the item's animation doesn't do damage
+            item.knockBack = 2;
+            item.rare = 5;
+            item.UseSound = SoundID.Item11;
+            item.autoReuse = true;
+            item.shoot = 10; //idk why but all the guns in the vanilla source have this
+            item.shootSpeed = 12f;
+            item.useAmmo = AmmoID.Dart;
+            item.crit = 7;
+
+        }
+        public override void AddRecipes()  //How to craft this item
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
