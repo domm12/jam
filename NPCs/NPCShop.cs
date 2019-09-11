c64f342d3fcdecb9fe9dbbf0f03c4268978397db
pasgen <36134204+pasgen@users.noreply.github.com>
2019-09-11 22:22:42 +0300

    Update NPCShop.cs

--- NPCs/NPCShop.cs
+++ NPCs/NPCShop.cs
@@ -9,6 +9,14 @@ namespace jam.NPCs
     {
         public override void NPCLoot(NPC npc)
         {
+            if (npc.type == NPCID.BigMimicHallow)
+            {
+                if (Main.rand.Next(5) == 0)   //item rarity 1/5 = 20%
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("dart_machine_gun"));
+                }
+            }
+            
             if (npc.type == 59)
             {
                 if (Main.rand.Next(20) == 0)
