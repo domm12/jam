f266f16a14cfa2f27afdf71849fd50b0d7b396ad
pasgen <36134204+pasgen@users.noreply.github.com>
2019-09-13 21:10:48 +0300

    Update dart_machine_gun.cs

--- Items/dart_machine_gun.cs
+++ Items/dart_machine_gun.cs
@@ -28,7 +28,6 @@ namespace jam.Items
             item.noMelee = true; //so the item's animation doesn't do damage
             item.knockBack = 2;
             item.rare = 5;
-            item.UseSound = SoundID.Item11;
             item.autoReuse = true;
             item.shoot = 10; //idk why but all the guns in the vanilla source have this
             item.shootSpeed = 12f;
