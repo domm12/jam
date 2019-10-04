8455a32ef1421b67a307b44d02efc652cc4f2dc6
domm12 <51050256+domm12@users.noreply.github.com>
2019-10-04 18:48:26 +0300

    Add files via upload

--- Projectiles/celestial_dagger_projectile.cs
+++ Projectiles/celestial_dagger_projectile.cs
@@ -20,67 +20,18 @@ namespace jam.Projectiles
             projectile.extraUpdates = 1;
             projectile.timeLeft = 200;
             projectile.light = 1f;
-            projectile.tileCollide = false;
+            projectile.tileCollide = true;
         }
         public override void AI()
         {
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-            if (Main.rand.Next(2) == 0) // this is how many duspt particles will spawn
-            {// DustID.Fire is a vanilla terrraria dust, change it to what you like. To add a modded dust the change DustID.Fire with mod.DustType("DustName")
-                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, BuffID.WaterCandle, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
-            }
-        }
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            /*if(projectile.penetrate == 0)
-            {
-                projectile.Kill();
-            }
-            else
-            {
-                projectile.penetrate--;
-            }
-            */
-            return false;
-        }
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) //When you hit an NPC
-        {
-
-            //target.AddBuff(24, 510);    //this adds a buff to the npc hit. 210 it the time of the buff
-
         }
         public override void Kill(int timeLeft)
         {
-            int rand = Main.rand.Next(0,3);
-            if(rand == 0){
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
-            }
-            if(rand == 1){
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
-            }
-            if(rand == 2){
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
-                int proj = Projectile.NewProjectile(projectile.position.X, projectile.position.Y - 300f, 0, 10, ProjectileID.HarpyFeather, (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
-                Main.projectile[proj].friendly = true;
-                Main.projectile[proj].timeLeft = 150;
-                Main.projectile[proj].tileCollide = false;
+            int rand = Main.rand.Next(1, 4);
+            for (int i = 0; i < rand; i++)
+            {
+                Projectile.NewProjectile(projectile.position.X+ i * 80, projectile.position.Y - 300f, 0, 8, mod.ProjectileType("HarpFeather"), (int)(20), projectile.knockBack, Main.myPlayer); // This spawns a projectile after this projectile is dead
             }
         }
     }
