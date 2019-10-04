b3638d3151c40477622b960895f50abbf0c0898a
domm12 <51050256+domm12@users.noreply.github.com>
2019-10-04 18:47:30 +0300

    Add files via upload

--- Projectiles/spectral_knife_projectile.cs
+++ Projectiles/spectral_knife_projectile.cs
@@ -18,11 +18,12 @@ namespace jam.Projectiles
             projectile.height = 16;
             projectile.friendly = true;
             projectile.thrown = true;
-            projectile.penetrate = 2;      //this is how many enemy this projectile penetrate before desapear
+            projectile.penetrate = 1;      //this is how many enemy this projectile penetrate before desapear
             projectile.extraUpdates = 1;
             projectile.timeLeft = 250;
             projectile.light = 1f;
             projectile.tileCollide = false;
+            projectile.alpha = 160;
         }
         public override void AI()
         {
