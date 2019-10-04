e7391a45ffeed8d8aa4bb8641801bac9bbac9336
domm12 <51050256+domm12@users.noreply.github.com>
2019-10-04 18:47:45 +0300

    Add files via upload

---
+++ Projectiles/LightShurikenP.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace jam.Projectiles
+{
+    public class LightShurikenP : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            projectile.width = 38;
+            projectile.height = 38;
+            projectile.friendly = true;
+            projectile.thrown = true;
+            projectile.penetrate = 2;      //this is how many enemy this projectile penetrate before desapear
+            projectile.extraUpdates = 1;
+            projectile.timeLeft = 200;
+            projectile.light = 1.5f;
+            projectile.tileCollide = true;
+        }
+        public override void AI()
+        {
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+            if (Main.rand.Next(2) == 0) // this is how many duspt particles will spawn
+            {// DustID.Fire is a vanilla terrraria dust, change it to what you like. To add a modded dust the change DustID.Fire with mod.DustType("DustName")
+                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 6, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+            }
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(189, 80); 
+        }
+    }
+}
