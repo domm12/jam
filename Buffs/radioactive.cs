ab5f68890b0c8f888a709567654f75d392b89d05
pasgen <36134204+pasgen@users.noreply.github.com>
2019-10-20 13:48:00 +0300

    Add files via upload

---
+++ Buffs/radioactive.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace jam.Buffs
+{
+    public class radioactive : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Radioactive");
+            Description.SetDefault("Wow, you are glowing");
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.poisoned = true;
+            MyPlayer.isRadioactive = true;
+            player.AddBuff(BuffID.Shine, 1);
+        }
+    }
+}
