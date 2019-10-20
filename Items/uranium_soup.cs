bc2434deb3aa8a90d583ea5cc72420547fad55ff
pasgen <36134204+pasgen@users.noreply.github.com>
2019-10-20 13:50:15 +0300

    Add files via upload

---
+++ Items/uranium_soup.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace jam.Items
+{
+    public class uranium_soup : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Soup with uranium");
+            Tooltip.SetDefault("Pure insanty...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.useTurn = true;
+            item.maxStack = 30;
+            item.rare = 1;
+            item.useAnimation = 17;
+            item.useTime = 17;
+            item.useStyle = 2;
+            item.UseSound = SoundID.Item3;
+            item.consumable = true;
+            item.buffType = mod.BuffType("radioactive");
+            item.buffTime = 2 * (60 * 60 * 60); // 2 hours
+            item.value = Item.sellPrice(0, 0, 0, 1);
+
+            
+        }
+
+        public override void AddRecipes()
+        {
+            
+        }
+    }
+}
